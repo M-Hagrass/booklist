17bbcad6754686f32ad601ac7e4d514cdc3a3af1
Mohamed Hagras <43122483+M-Hagrass@users.noreply.github.com>
2020-10-25 04:33:49 +0200

    Add MVC structure

---
+++ app.js
@@ -0,0 +1,46 @@
+(()=>{
+  // const model = {
+  // }
+
+  const controller = {
+    init: function(){
+      formView.init();
+    },
+
+
+
+  }
+
+  const formView = {
+    init: function (){
+
+    },
+
+    render: function(){
+
+
+    }
+  }
+
+  const resultView = {
+    init: function(){
+
+    },
+
+    render: function(){
+
+    }
+  }
+
+  const warningView = {
+    init: function(message, className){
+
+    },
+
+    render: function(){
+
+    }
+  }
+
+  controller.init();
+})()
